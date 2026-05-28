afb913af56d89157247a631442a4675697445a1f
Nauannems <nauannesantosmarinho@email.com>
2026-05-28 11:27:58 -0300

    parte final do banco servidor

--- BancoServidor.java
+++ BancoServidor.java
@@ -21,7 +21,7 @@ public class BancoServidor {
             System.out.println("Erro ao iniciar o servidor: " + e.getMessage());
         }
     }
-    // Classe que gerencia a comunicação com um cliente específico
+
     private static class GerenciadorCliente implements Runnable {
         private Socket conexao;
 
@@ -36,12 +36,48 @@ public class BancoServidor {
                     PrintWriter saida = new PrintWriter(conexao.getOutputStream(), true)
             ) {
                 String dados;
-                // Lê as mensagens enviadas pelo cliente
                 while ((dados = entrada.readLine()) != null) {
-                    // Separa a mensagem recebida: ACAO;CONTA;VALOR
+
                     String[] partes = dados.split(";");
                     int opcao = Integer.parseInt(partes[0]);
                     String conta = partes.length > 1 ? partes[1] : "";
                     double valor = partes.length > 2 ? Double.parseDouble(partes[2]) : 0.0;
 
                     String resposta = "erro;Ação inválida";
+                    if (!contas.containsKey(conta)) {
+                        resposta = "erro;Conta não encontrada";
+                    } else {
+                        if (opcao==1) {
+                            resposta = "sucesso;" + contas.get(conta);
+                        } else if (opcao==2) {
+                            if (valor > 0) {
+                                contas.put(conta, contas.get(conta) + valor);
+                                resposta = "sucesso;" + contas.get(conta);
+                            } else {
+                                resposta = "erro;Valor inválido";
+                            }
+                        } else if (opcao==3) {
+                            if (valor > 0 && contas.get(conta) >= valor) {
+                                contas.put(conta, contas.get(conta) - valor);
+                                resposta = "sucesso;" + contas.get(conta);
+                            } else {
+                                resposta = "erro;Saldo insuficiente ou valor inválido";
+                            }
+                        }
+                    }
+                    saida.println(resposta);
+                }
+            } catch (Exception e) {
+                System.out.println("Erro na conexão: " + e.getMessage());
+            } finally {
+                try {
+                    System.out.println("Conexão encerrada com " + conexao.getInetAddress().getHostAddress());
+                    conexao.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
